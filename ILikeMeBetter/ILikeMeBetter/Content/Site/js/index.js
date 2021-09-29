6c97b494d69ad73ca3b6d1792c7ce128926fb135
levietvy <levietvy199@gmail.com>
2021-09-29 11:04:19 +0700

    Add site's content folder, build index page.

---
+++ ILikeMeBetter/ILikeMeBetter/Content/Site/js/index.js
@@ -0,0 +1,9 @@
+<script>
+    document.addEventListener( 'DOMContentLoaded', function() {
+        new Splide('#splide', {
+            type: 'loop',
+            perPage: 3,
+            perMove: 1,
+        }).mount()
+    } );
+</script>
---
+++ ILikeMeBetter/ILikeMeBetter/Content/Site/js/job-detail.js
@@ -0,0 +1,237 @@
+let job = [
+    {
+        id:'',
+        jobName:'FE React',
+        salary:'Up to $1600',
+        location:'Da Nang',
+        companyName:'Global IT',
+        requiment:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        imageCompany:'http://placehold.it/400x300',
+        JD:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        sex:true,
+        jobType:"FullTime",
+        age:''
+    },
+    {
+        id:'',
+        jobName:'FE React',
+        salary:'Up to $1600',
+        location:'Da Nang',
+        companyName:'Global IT',
+        requiment:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        imageCompany:'http://placehold.it/400x300',
+        JD:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        sex:true,
+        jobType:"FullTime"
+    },
+    {
+        id:'',
+        jobName:'FE React',
+        salary:'Up to $1600',
+        location:'Da Nang',
+        companyName:'Global IT',
+        requiment:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        imageCompany:'http://placehold.it/400x300',
+        JD:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        sex:true,
+        jobType:"FullTime"
+    },
+    {
+        id:'',
+        jobName:'FE React',
+        salary:'Up to $1600',
+        location:'Da Nang',
+        companyName:'Global IT',
+        requiment:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        imageCompany:'http://placehold.it/400x300',
+        JD:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        sex:true,
+        jobType:"FullTime"
+    },
+    {
+        id:'',
+        jobName:'FE React',
+        salary:'Up to $1600',
+        location:'Da Nang',
+        companyName:'Global IT',
+        requiment:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        imageCompany:'http://placehold.it/400x300',
+        JD:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        sex:true,
+        jobType:"FullTime"
+    },
+    {
+        id:'',
+        jobName:'FE React',
+        salary:'Up to $1600',
+        location:'Da Nang',
+        companyName:'Global IT',
+        requiment:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        imageCompany:'http://placehold.it/400x300',
+        JD:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        sex:true,
+        jobType:"FullTime"
+    },
+    {
+        id:'',
+        jobName:'FE React',
+        salary:'Up to $1600',
+        location:'Da Nang',
+        companyName:'Global IT',
+        requiment:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        imageCompany:'http://placehold.it/400x300',
+        JD:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        sex:true,
+        jobType:"FullTime"
+    },
+    {
+        id:'',
+        jobName:'FE React',
+        salary:'Up to $1600',
+        location:'Da Nang',
+        companyName:'Global IT',
+        requiment:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        imageCompany:'http://placehold.it/400x300',
+        JD:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        sex:true,
+        jobType:"FullTime"
+    },
+    {
+        id:'',
+        jobName:'FE React',
+        salary:'Up to $1600',
+        location:'Da Nang',
+        companyName:'Global IT',
+        requiment:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        imageCompany:'http://placehold.it/400x300',
+        JD:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        sex:true,
+        jobType:"FullTime"
+    },
+    {
+        id:'',
+        jobName:'FE React',
+        salary:'Up to $1600',
+        location:'Da Nang',
+        companyName:'Global IT',
+        requiment:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        imageCompany:'http://placehold.it/400x300',
+        JD:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        sex:true,
+        jobType:"FullTime"
+    },
+    {
+        id:'',
+        jobName:'FE React',
+        salary:'Up to $1600',
+        location:'Da Nang',
+        companyName:'Global IT',
+        requiment:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        imageCompany:'http://placehold.it/400x300',
+        JD:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        sex:true,
+        jobType:"FullTime"
+    },
+    {
+        id:'',
+        jobName:'FE React',
+        salary:'Up to $1600',
+        location:'Da Nang',
+        companyName:'Global IT',
+        requiment:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        imageCompany:'http://placehold.it/400x300',
+        JD:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        sex:true,
+        jobType:"FullTime"
+    },
+    {
+        id:'',
+        jobName:'FE React',
+        salary:'Up to $1600',
+        location:'Da Nang',
+        companyName:'Global IT',
+        requiment:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        imageCompany:'http://placehold.it/400x300',
+        JD:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        sex:true,
+        jobType:"FullTime"
+    },
+    {
+        id:'',
+        jobName:'FE React',
+        salary:'Up to $1600',
+        location:'Da Nang',
+        companyName:'Global IT',
+        requiment:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        imageCompany:'http://placehold.it/400x300',
+        JD:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        sex:true,
+        jobType:"FullTime"
+    },
+    {
+        id:'',
+        jobName:'FE React',
+        salary:'Up to $1600',
+        location:'Da Nang',
+        companyName:'Global IT',
+        requiment:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        imageCompany:'http://placehold.it/400x300',
+        JD:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        sex:true,
+        jobType:"FullTime"
+    },
+    {
+        id:'',
+        jobName:'FE React',
+        salary:'Up to $1600',
+        location:'Da Nang',
+        companyName:'Global IT',
+        requiment:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        imageCompany:'http://placehold.it/400x300',
+        JD:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        sex:true,
+        jobType:"FullTime"
+    },
+    {
+        id:'',
+        jobName:'FE React',
+        salary:'Up to $1600',
+        location:'Da Nang',
+        companyName:'Global IT',
+        requiment:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        imageCompany:'http://placehold.it/400x300',
+        JD:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis at ex repellat modi incidunt laboriosam accusamus culpa, non aliquam eos distinctio suscipit, consectetur exercitationem ratione quod fuga provident minima? Nesciunt?',
+        sex:true,
+        jobType:"FullTime"
+    }
+    
+]
+
+var list_jobs = document.getElementById('list-jobs')
+
+renderListJobs = (job) => {
+    job.forEach(element => {
+        var jobs = `
+        <div class="col-sm-4 ">
+        <div class="card-deck mb-3">
+            <div class="job-card card">
+                <img class="card-img-top " src="img/16.png" alt="">
+                <div class="card-body job-des">
+                    <h5 class="card-title job-name">${element.jobName}</h5>
+                    <p class="card-text salary">${element.salary}/ tháng</p>
+                    <span class="job-location">
+                        <i class="fas fa-map-marker-alt"></i>
+                        ${element.location}
+                    </span>
+                </div>
+                <div class="add-job">
+                    <i class="far fa-heart"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+        `
+    list_jobs.insertAdjacentHTML("beforeend", jobs)
+    });
+}
+renderListJobs(job)
